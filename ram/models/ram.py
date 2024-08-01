70085b791466ab990077466f2b37784a551dfe66
dahwin <116961692+dahwin@users.noreply.github.com>
2024-08-02 01:22:07 +0600

    r

--- ram/models/ram.py
+++ ram/models/ram.py
@@ -2274,8 +2274,6 @@ from typing import List
 from transformers import BertTokenizer
 from urllib.parse import urlparse
 from timm.models.hub import download_cached_file
-from .vit import interpolate_pos_embed
-
 from pathlib import Path
 
 CONFIG_PATH=(Path(__file__).resolve().parents[1])
