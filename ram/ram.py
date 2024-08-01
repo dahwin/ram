2285fa03b49d3456171e532b02a6ec518238977e
dahwin <116961692+dahwin@users.noreply.github.com>
2024-08-02 03:54:30 +0600

    Update ram.py

--- ram/ram.py
+++ ram/ram.py
@@ -5,6 +5,7 @@ import sys
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(__dir__)
 sys.path.insert(0, os.path.abspath(os.path.join(__dir__, "../..")))
+print(os.path.abspath(os.path.join(__dir__, "../..")))
 import json
 import warnings
 
