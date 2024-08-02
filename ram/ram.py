1dc238444727c123bbcf1bdb7d481d46fbbbaca8
dahwin <116961692+dahwin@users.noreply.github.com>
2024-08-02 17:30:52 +0600

    q

--- ram/ram.py
+++ ram/ram.py
@@ -6,6 +6,7 @@ __dir__ = os.path.dirname(os.path.abspath(__file__))
 # sys.path.append(__dir__)
 # sys.path.insert(0, os.path.abspath(os.path.join(__dir__,  "./")))
 pa = os.path.abspath(os.path.join(__dir__,  "./"))
+print(pa)
 pathh = f"{pa}/conf"
 
 
